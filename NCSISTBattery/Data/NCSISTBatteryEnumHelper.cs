d39c04c70916e3604d5bbfc632d986ffefbb78dc
Peter Yuan <Peter.Yuan@Tm-robot.com>
2025-04-02 17:46:34 +0800

    update material style

---
+++ NCSISTBattery/Data/NCSISTBatteryEnumHelper.cs
@@ -0,0 +1,24 @@
+﻿using CommonLibraryP.Data;
+using CommonLibraryP.MachinePKG;
+using DevExpress.Blazor;
+
+namespace NCSISTBattery.Data
+{
+    public static class NCSISTBatteryEnumHelper
+    {
+        public static IEnumerable<HeatpieceStyleWrapperClass> GetHeatpieceStylesWrapperClass()
+        {
+            return Enum.GetValues(typeof(ButtonRenderStyle)).OfType<ButtonRenderStyle>()
+                .Select(x => new HeatpieceStyleWrapperClass(x));
+        }
+    }
+
+    public class HeatpieceStyleWrapperClass : EnumWrapper
+    {
+        public HeatpieceStyleWrapperClass(ButtonRenderStyle renderStyle)
+        {
+            index = (int)renderStyle;
+            displayName = renderStyle.ToString();
+        }
+    }
+}
